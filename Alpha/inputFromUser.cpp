0f7bc08a923a51da2456c188db504829440dd0f1
Its-Delimas <spencerdelimas@gmail.com>
2025-11-22 17:16:07 +0300

    taking input from the User

---
+++ Alpha/inputFromUser.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+    int num;
+    cout <<"Please enter a number"<<endl;
+    cin>>num;
+
+    cout<<"Number entered is: "<<num<<endl;
+    
+    return 0;
+}
