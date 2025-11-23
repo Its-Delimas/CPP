a2aca3234b33af98af1b8d9401472a9fce8a47ee
Its-Delimas <spencerdelimas@gmail.com>
2025-11-24 00:03:14 +0300

    linked lists~ correction

--- Alpha/linkedList.cpp
+++ Alpha/linkedList.cpp
@@ -1,24 +1,45 @@
 #include <iostream>
 #include <string>
+
 using namespace std;
 
-struct Node{
-    int marks;
-    string name;
-    Node* next;
-    Node(int val,string s):marks(val),name(s),next(nullptr){}
+struct Node {
+  int id;
+  string name;
+  Node *next;
+  Node(int patientId, const string &patientname)
+      : id(patientId), name(patientname), next(nullptr) {}
 };
 
-int main(){
-    Node* head= new Node(30,"spencer");
-    head->next=new Node(40,"Moreene");
-    head->next->next=new Node(50,"Teddybears");
-    head->next->next->next=new Node(60,"Legend");
-    
-    Node* obj=head;
-    while(obj!=nullptr){
-        cout<<obj->marks<<" : "<<obj->name<<" , ";
-        obj=obj->next;
-    }
-    return 0;
+void insertAtEnd(Node *&head, int patientId, const string &patientname) {
+  Node *newNode = new Node(patientId, patientname);
+
+  if (head == nullptr) {
+    head = newNode;
+  }
+
+  Node *temp = head;
+  while (temp->next != nullptr) {
+    temp = temp->next;
+  }
+  temp->next = newNode;
+}
+
+void displayPatients(Node *head) {
+  Node *temp = head;
+  while (temp != nullptr) {
+    cout << "ID: " << temp->id << ", Name" << temp->name << " " << endl;
+    temp = temp->next;
+  }
+}
+
+int main() {
+  Node *head = nullptr;
+
+  insertAtEnd(head, 5463, "Spencer");
+  insertAtEnd(head, 102, "Bob");
+  insertAtEnd(head, 103, "Charlie");
+
+  displayPatients(head);
+  return 0;
 }
